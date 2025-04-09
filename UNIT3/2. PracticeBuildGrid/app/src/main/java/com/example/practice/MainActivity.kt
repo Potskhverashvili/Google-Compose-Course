bd770eac15b276b3fa2aafb4c6adc80d6f6d7f6c
GFO <gia.focxverashvili@gmail.com>
2025-04-09 17:14:32 +0400

    LazyVerticalCard add

--- UNIT3/2. PracticeBuildGrid/app/src/main/java/com/example/practice/MainActivity.kt
+++ UNIT3/2. PracticeBuildGrid/app/src/main/java/com/example/practice/MainActivity.kt
@@ -5,26 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.practice.data.DataSource
 import com.example.practice.model.Topic
 import com.example.practice.ui.theme.PracticeTheme
 
@@ -34,8 +40,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracticeTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    TopicApp()
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TopicGrid(
+                        modifier = Modifier.padding(
+                            start = 8.dp,
+                            top = 8.dp,
+                            end = 8.dp
+                        )
+                    )
                 }
             }
         }
@@ -43,43 +60,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TopicApp() {
+fun TopicGrid(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        items(DataSource.topics) { topic ->
+            TopicCard(topic)
+        }
+    }
+
 }
 
 @Composable
 fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier
-            .clip(shape = RoundedCornerShape(12.dp))
-            .background(color = Color(0xFFC5C5C5))
-    ) {
-        Image(
-            painter = painterResource(topic.imageResourceId),
-            contentDescription = stringResource(topic.stringResourceId),
-            Modifier.size(height = 68.dp, width = 68.dp)
-        )
+    Card {
+        Row {
+            // ------------------------- Image ----------------------------
+            Box {
+                Image(
+                    painter = painterResource(topic.imageRes),
+                    contentDescription = stringResource(topic.name),
+                    modifier = modifier
+                        .size(height = 68.dp, width = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
+                )
+            }
 
-        Column(
-            modifier = Modifier.padding(top = 16.dp, end = 16.dp)
-        ) {
-            Text(
-                text = stringResource(topic.stringResourceId),
-                fontWeight = FontWeight.Medium,
-                modifier = Modifier.padding(start = 16.dp),
-            )
-
-            Row {
-                Icon(
-                    painter = painterResource(R.drawable.ic_relation),
-                    contentDescription = null,
-                    modifier = Modifier.padding(start = 16.dp, end = 8.dp)
-                    )
-
+            // ------------------------ Texts and Logo -----------------------
+            Column {
+                // -------------- Name -----------------
                 Text(
-                    text = topic.amountOfRelations.toString(),
-                    fontWeight = FontWeight.Medium
+                    text = stringResource(topic.name),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(
+                        start = 16.dp,
+                        end = 16.dp,
+                        top = 16.dp,
+                        bottom = 8.dp
+                    ),
+                    maxLines = 1
                 )
 
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    // ----------- Logo ------------
+                    Icon(
+                        painter = painterResource(R.drawable.ic_relation),
+                        contentDescription = null,
+                        modifier = Modifier.padding(start = 16.dp)
+                    )
+
+                    // ---------- Available  Course ----------------
+                    Text(
+                        text = topic.availableCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+
+                }
             }
         }
     }
@@ -89,12 +130,13 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
 @Composable
 fun TopicsAppPreview() {
     PracticeTheme {
-        TopicCard(
-            Topic(
-                stringResourceId = R.string.photography,
-                imageResourceId = R.drawable.photography,
-                amountOfRelations = 58,
-            )
-        )
+        val topic = Topic(R.string.photography, 321, R.drawable.photography)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TopicCard(topic = topic)
+        }
     }
 }
